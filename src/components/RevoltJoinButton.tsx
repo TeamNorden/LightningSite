900236bb54055c4f7c538b11af2f2cdacc87c4f5
Codeize <mrcodeize@gmail.com>
2022-02-05 12:52:27 +0000

    Made the button a graident

--- src/components/RevoltJoinButton.tsx
+++ src/components/RevoltJoinButton.tsx
@@ -5,7 +5,7 @@ export const RevoltJoinButton: React.FC = () => {
     return (
         <Link href={REVOLT_INVITE}>
             <a
-                className={`bg-revolt text-black hover:opacity-70 py-2 w-[10rem] text-[1rem] lg:p-3 lg:w-[14rem] lg:text-[1.3rem] rounded-full font-bold grid place-items-center`}
+                className={`button_cta bg-revolt text-black hover:opacity-70 py-2 w-[10rem] text-[1rem] lg:p-3 lg:w-[14rem] lg:text-[1.3rem] rounded-full font-bold grid place-items-center`}
             >
                 Join Us on Revolt
             </a>
